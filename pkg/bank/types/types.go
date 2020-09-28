10649a41e59fcca731b33775d35372832c575e20
olegro <mygoog2000@mail.ru>
2020-09-29 02:23:35 +0500

    инициализирован модуль

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,20 @@
+package types
+
+// Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д).
+type Money int64
+
+// Category представляет собой категорию, в которой был совершен платёж (авто, аптека, рестораны и т.д).
+type Category string
+
+// Status представляет статус плятежей.
+type Status string
+
+
+
+// Payment представляет информацию о платеже.
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+	Status   Status
+}
