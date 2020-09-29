7198b70f406ae2924a289b474ddb7b16c20ba9c4
olegro <mygoog2000@mail.ru>
2020-09-29 07:14:01 +0500

    добавлены статусы платежа

--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -6,13 +6,20 @@ type Money int64
 // Category представляет собой категорию, в которой был совершен платёж (авто, аптека, рестораны и т.д).
 type Category string
 
-// Status представляет статус плятежей.
+// Status представляет статус платежей.
 type Status string
 
+// Предопределенные статусы платежа.
+const (
+	StatusOk       Status = "OK"
+	StatusFail     Status = "FAIL"
+	StatusProgress Status = "INPROGRESS"
+)
 
 // Payment представляет информацию о платеже.
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status   Status
 }
